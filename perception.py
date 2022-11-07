dc89e9d5df9d15ff28149fc379ed831ebc6411a1
Giuseppe Di Palma <dipalmagiuseppe1@gmail.com>
2022-11-07 16:36:12 +0100

    remove python project dir

--- perception.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-from flask import Flask, request
-"""This module provides Main entry point script."""
-app = Flask(__name__)
-from src.main import main
-
-if __name__ == "__main__":
-    main()
--- src/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request
-
-
-@app.route('/titles', methods=['POST'])
-
-def main():
-    print("Iniziamo")
-    print(request.form['POST'])
-    return "Received"
